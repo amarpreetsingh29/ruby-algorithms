750a1d8218ef55e531f0b08f3e03a2872d6faefb
Amarpreet Singh <amarpreet.talwar@gmail.com>
2024-06-10 15:34:50 -0700

    equilbrium point in array

---
+++ array/h_rank/equilibrium.rb
@@ -0,0 +1,33 @@
+def equilibrium_point(a)
+  index = -1
+  before_sum = 0
+  n = a.length
+
+  (0..n-1).each do |i|
+    before_sum += a[i-1] if i>0
+
+    after_sum = 0
+    (i+1..n-1).each do |j|
+      after_sum += a[j]
+
+      if j == n-1 && before_sum == after_sum
+        index = i
+        return index
+      end
+
+      if after_sum > before_sum
+        break
+      end
+    end
+  end
+
+  index
+end
+
+
+# result = equilibrium_point([1,3,5,2,2])
+# puts "Equilibrium index is #{result}"
+
+
+result = equilibrium_point([1])
+puts "Equilibrium index is #{result}"
