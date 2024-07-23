f3f27a2fe69159aee9384ec0d27a56cc2402e47c
Amarpreet Singh <amarpreet.talwar@gmail.com>
2024-07-22 18:05:11 -0700

    max occuring char

---
+++ string/h_rank/max_occuring_char_lexicographic.rb
@@ -0,0 +1,25 @@
+def max_occuring_char(s)
+  max_count = 1
+  max_char = nil
+  temp = {}
+  s.each_char do |char|
+    if temp[char] == nil
+      temp[char] = 1
+    else
+      temp[char] += 1
+      if temp[char] > max_count
+        max_count = temp[char]
+        max_char = char
+      elsif temp[char] == max_count
+        if char < max_char
+          max_count = temp[char]
+          max_char = char
+        end
+      end
+    end
+  end
+  print max_char
+end
+
+input= 'aabbcc'
+max_occuring_char(input)
